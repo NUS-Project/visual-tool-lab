ba2eeff4315eaa48f3c50120f19dfa548fed9d9f
NUS-Project <2022212084@stu.hit.edu.cn>
2025-12-27 04:28:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ def load_data(dataset):
     # examplers = []
 
     test_path = f'{dataset}/test.jsonl'
-    with open(test_path, 'r') as file:
+    with open(test_path, 'r', encoding='utf-8') as file:
         for line in file:
             test_qa.append(json.loads(line))
 
     # train_path = f'{dataset}/train.jsonl'
-    # with open(train_path, 'r') as file:
-    #     for line in file:
+    # with open(train_path, encoding='utf-8') as file:
+    #     for count, line in enumerate(file):
+    #         if count == 5:  # 只读取前5行
+    #             break
     #         examplers.append(json.loads(line))
 
     return test_qa
@@ -37,15 +39,18 @@ def create_question(sample, dataset):
     return sample['question']
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--general_model_path', type=str, default='/z_data/pretrained/')
-parser.add_argument('--dataset_path', type=str, default='/z_data/yunhang/MedData/')
+parser.add_argument('--general_model_path', type=str, default=r'E:\Code')
+parser.add_argument('--dataset_path', type=str, default=r'E:\Code\MedData')
 parser.add_argument('--dataset_name', type=str, default='medqa')
 parser.add_argument('--model', type=str, default='medagents',choices=['Qwen2.5-VL-72B-Instruct','Qwen2.5-72B-Instruct','medagents','Llama-3.3-70B-Instruct'])
-parser.add_argument('--root_path', type=str, default='/z_data/yunhang/Code/MedToolLab')
-parser.add_argument("--device", type=str, default="cuda:6", help='Device / device_map, e.g. "cuda:3" or "auto"')
-parser.add_argument('--num_samples', type=int, default=10000)
+parser.add_argument('--root_path', type=str, default=r'E:\Code\MedToolLab')
+parser.add_argument("--device", type=str, default="auto", help='Device / device_map, e.g. "cuda:3" or "auto"')
+parser.add_argument('--num_samples', type=int, default=5)
 args = parser.parse_args()
 
+os.environ["OPENAI_API_KEY"] = "sk-i5zo6MXMbPdCaPK6gP8Px1ZJQbKZysEAqVmUnWPLJkGFeFpJ"
+os.environ["BASE_URL"] = 'https://yinli.one/v1' #"https://hiapi.online/v1"
+os.environ["GENERAL_MODEL_PATH"] = args.general_model_path
 # MODEL_API_CONFIG_PATH = os.environ["MODEL_API_CONFIG_PATH"]
 model_paths = os.path.join(args.general_model_path, args.model)
 dataset_path_name = os.path.join(args.dataset_path, args.dataset_name)
@@ -80,7 +85,7 @@ for no, sample in enumerate(tqdm(test_qa)):
     elif "Qwen" in args.model:
         final_decision = Qwen_model.chat(question)
     elif args.model=="medagents":
-        final_decision = mdagents_test(question, args.root_path, args)
+        final_decision = mdagents_test(question, args.root_path)
         
     correct_answer_idx = sample['answer_idx']
     correct_answer_content = sample['options'][correct_answer_idx]
